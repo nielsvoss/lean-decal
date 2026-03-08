bc0c5c06dcb98f2e124b004a0026a4c3ae6770ae
Niels Voss <code@nielsvoss.com>
2026-03-07 22:06:25 -0800

    Start work on HW1

---
+++ HWDraft/HW1.lean
@@ -0,0 +1,21 @@
+
+def triangleNumber (n : Nat) : Nat := 0
+
+def pascalsTriangle := 0
+
+/-
+Return the sum of the largest two elements of xs.
+If xs is empty, return 0, and if xs has exactly one element, return that element.
+-/
+def sumLargestTwo (xs : List Nat) : Nat :=
+  0
+
+def bubbleSort (xs : List Nat) : List Nat :=
+  []
+
+/-
+Implement sqrt using Newton's method.
+Return 0 for negative numbers.
+-/
+def sqrt (x : Float) (tolerance : Float) (fuel : Nat) : Float :=
+  0 -- placeholder
