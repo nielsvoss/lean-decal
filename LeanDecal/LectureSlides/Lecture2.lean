20aa2a28995588b93ad393a1b6a2ccb23c5baee8
Niels Voss <code@nielsvoss.com>
2026-06-15 21:35:20 -0700

    Start working on Lecture 2

--- LeanDecal/LectureSlides/Lecture2.lean
+++ LeanDecal/LectureSlides/Lecture2.lean
@@ -3,8 +3,59 @@ import Verso.Doc.Concrete
 
 open VersoSlides
 
+set_option verso.slides.panel false
+
 #doc (Slides) "Lecture 2" =>
 
-# Test
+# Lecture 2: Dependent Type Theory
 
-Placeholder slideshow content
+# Simple Type Theory
+
+# Basic Types
+
+:::::attr (style := "font-size: 0.52em")
+::::table +colHeaders +border +rowSeps +colSeps
+*
+  * Type
+  * Description
+  * Example elements
+*
+  * {name}`Nat`
+  * :::attr (style := "font-size: 0.52em")
+    The natural numbers/nonnegative integers. Can be arbitrarily large.
+    :::
+  * `0, 1, 2, 3, 4, ...`
+*
+  * {name}`Int`
+  * The integers
+  * `..., -2, -1, 0, 1, 2, ...`
+*
+  * {name}`String`
+  * Unicode text
+  * `"apple"`, `"LeanCal"`
+*
+  * {name}`Float`
+  * The 64-bit floating point numbers (IEEE 754)
+  * `3.2, 6.0, -0.0, NaN`
+*
+  * {name}`Bool`
+  * A known truth value
+  * `true, false`
+*
+  * {lean}`Prop`
+  * An unknown truth value
+  * `True`, `False`, `RiemannHypothesis`
+::::
+:::::
+
+# Unit Type
+
+The {name}`Unit` type has exactly one element, denoted {lean}`()`.
+
+An element of {name}`Unit` carries $`0` bits of information.
+
+```lean
+#check ()
+```
+
+Ex: {name}`List.length` is a bijection from {lean}`List Unit` to {name}`Nat`.
--- LectureSlides.lean
+++ LectureSlides.lean
@@ -5,7 +5,7 @@ open VersoSlides
 
 def main : IO UInt32 := do
   let returnCode2 ← slidesMain
-    (config := { theme := "black", slideNumber := true, transition := "slide" })
+    (config := { theme := "white", slideNumber := true, transition := "slide" })
     (doc := %doc LeanDecal.LectureSlides.Lecture2)
   if returnCode2 ≠ 0 then return returnCode2
   IO.FS.rename "_slides/index.html" "_slides/Lecture2.html"
