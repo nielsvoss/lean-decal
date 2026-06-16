39cd819530e543a68e94f78a6bc5d91c2e3e459c
Niels Voss <code@nielsvoss.com>
2026-06-15 20:40:32 -0700

    Initialize a slideshow

---
+++ LeanDecal/LectureSlides/Lecture2.lean
@@ -0,0 +1,10 @@
+import VersoSlides
+import Verso.Doc.Concrete
+
+open VersoSlides
+
+#doc (Slides) "Lecture 2" =>
+
+# Test
+
+Placeholder slideshow content
---
+++ LectureSlides.lean
@@ -0,0 +1,13 @@
+import LeanDecal.LectureSlides.Lecture2
+import VersoSlides
+
+open VersoSlides
+
+def main : IO UInt32 := do
+  let returnCode2 ← slidesMain
+    (config := { theme := "black", slideNumber := true, transition := "slide" })
+    (doc := %doc LeanDecal.LectureSlides.Lecture2)
+  if returnCode2 ≠ 0 then return returnCode2
+  IO.FS.rename "_slides/index.html" "_slides/Lecture2.html"
+
+  return 0
