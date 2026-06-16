4501e8786a52281ce18027a8caa454ddd9780210
Niels Voss <code@nielsvoss.com>
2026-06-15 21:45:17 -0700

    Add more to lecture 2

--- LeanDecal/LectureSlides/Lecture2.lean
+++ LeanDecal/LectureSlides/Lecture2.lean
@@ -21,7 +21,7 @@ set_option verso.slides.panel false
   * Example elements
 *
   * {name}`Nat`
-  * :::attr (style := "font-size: 0.52em")
+  * :::attr (style := "font-size: 0.7em")
     The natural numbers/nonnegative integers. Can be arbitrarily large.
     :::
   * `0, 1, 2, 3, 4, ...`
@@ -59,3 +59,26 @@ An element of {name}`Unit` carries $`0` bits of information.
 ```
 
 Ex: {name}`List.length` is a bijection from {lean}`List Unit` to {name}`Nat`.
+
+# Empty Type
+
+The {name}`Empty` type has zero elements. Useful for representing unreachable branches of code.
+
+The function {name}`Empty.elim` converts a element of {name}`Empty` into whatever type you want.
+
+Slogan: An element of the {name}`Empty` type "carries infinity bits of information." (not to be taken literally)
+
+# The Function Type
+
+```lean -show
+opaque A : Type
+opaque B : Type
+```
+
+If {lean}`A` and {lean}`B` are types, then {lean}`A → B` is the type of functions from {lean}`A` to {lean}`B`.
+
+Each object assigns one element of {lean}`B` to every element of {lean}`A`.
+
+In Lean, functions must always return the same value every time they are called, may not have side effects, and must not enter an infinite loop.
+
+If {lean}`A` has 3 elements and {lean}`B` has 5 elements, then {lean}`A → B` has $`5^3 = 125` elements.
