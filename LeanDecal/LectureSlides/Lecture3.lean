18632a2388944e2c3d86cfa6717c569621b43641
Niels Voss <code@nielsvoss.com>
2026-06-17 21:50:41 -0700

    Slides about representing proofs

--- LeanDecal/LectureSlides/Lecture3.lean
+++ LeanDecal/LectureSlides/Lecture3.lean
@@ -6,6 +6,7 @@ open VersoSlides
 
 set_option verso.slides.panel false
 set_option linter.hashCommand false
+set_option linter.style.emptyLine false
 
 #doc (Slides) "Lecture 3" =>
 
@@ -20,27 +21,9 @@ false.
 
 Unlike {name}`Bool`, given a {lean}`Prop` we don't always know if it is true or false.
 
-/-
-# Operations on Prop
-
-```lean -show
--- TODO: Consider namespacing
-opaque p : Prop
-opaque q : Prop
-```
-
-{lean}`And p q` (also written as {lean}`p ∧ q`) is the _conjunction_ of {lean}`p` and {lean}`q`.
-```lean -stretch
-#check And
-```
-
-{lean}`Or p q` (also written as {lean}`p ∨ q`) is the _disjunction_ of {lean}`p` and {lean}`q`.
-```lean -stretch
-#check Or
-```
--/
-
 # Decidable Propositions
+
+:::attr (style := "font-size: 0.6em")
 
 Some propositions are _decidable_, which means that Lean can automatically convert them into a
 {name}`Bool`.
@@ -57,22 +40,23 @@ This fails for propositions that are not decidable:
 ```lean +error -stretch
 #check (RiemannHypothesis : Bool)
 ```
+:::
 
-# Representing proofs in Lean
+# Representing proofs
 
 ```lean -show
-namespace AlternateReality
+--namespace AlternateReality
 opaque Proof : Prop → Type
-opaque And : Prop → Prop → Prop
-opaque Or : Prop → Prop → Prop
-opaque Not : Prop → Prop
-opaque Implies : Prop → Prop → Prop
-axiom and_intro (p q : Prop) : Proof p → Proof q → Proof (And p q)
-axiom modus_ponens (p q : Prop) : Proof (Implies p q) → Proof p → Proof q
+--opaque And : Prop → Prop → Prop
+--opaque Or : Prop → Prop → Prop
+--opaque Not : Prop → Prop
+set_option linter.unusedVariables false in
+opaque Implies (a b : Prop) : Prop
+opaque p : Prop
+opaque q : Prop
 ```
 
-The most important thing about propositions is that they can be proven.
-
+:::attr (style := "font-size: 0.6em")
 In Lean, _proofs are mathematical objects_.
 This means you can have functions that take in proofs and ouptut proofs.
 
@@ -82,9 +66,68 @@ of proofs of the proposition `p`.
 ```lean -stretch
 #check Proof
 ```
-
 _Important: {name}`Proof` is not a real thing in Lean, we're just using it as an example._
+:::
+
+# Representing proofs
+
+:::attr (style := "font-size: 0.6em")
+We can then use {lean}`And p q`, {lean}`Or p q`, {lean}`Not p`, and {lean}`Implies p q` to build
+compound propositions.
+```lean -stretch
+#check And
+#check Or
+#check Not
+#check Implies
+```
+_Important: This is still hypothetical, not how Lean actually works._
+:::
+
+# And introduction rule
+
+:::attr (style := "font-size: 0.7em")
+In normal logic, the following is true:
+
+> From a proof of $`p` and a proof of $`q`, we can obtain a proof of $`p \land q`.
+
+We can represent this in Lean as follows:
+
+```lean -stretch
+axiom and_intro {p q : Prop} :
+  Proof p → Proof q → Proof (And p q)
+```
+_The `axiom` keyword in Lean is like `def` but you don't need to provide a value._
+:::
+
+# Modus ponens
+
+:::attr (style := "font-size: 0.7em")
+The principle of modus ponens states:
+
+> From a proof of $`p \Rightarrow q` and a proof of $`p`, we can obtain a proof of $`q`.
+
+Representing this in Lean:
+```lean -stretch
+axiom modus_ponens {p q : Prop} :
+  Proof (Implies p q) → Proof p → Proof q
+```
+:::
+
+# Our first proof
+
+Our first proof of the following theorem:
+> If $`p \Rightarrow q` and $`p \Rightarrow r` and $`p`, then $`q \land r`.
 
 ```lean -show
-end AlternateReality
+noncomputable section
+```
+```lean +panel
+-----
+def my_proof (p q r : Prop) :
+    Proof (Implies p q) → Proof (Implies p r)
+    → Proof p → Proof (And q r) :=
+  fun h₁ h₂ h₃ ↦
+    and_intro
+      (modus_ponens h₁ h₃)
+      (modus_ponens h₂ h₃)
 ```
