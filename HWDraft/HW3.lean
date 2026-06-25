36f0c25f280c520da70a67f6eda4587f9386fc65
Niels Voss <code@nielsvoss.com>
2026-06-24 21:51:38 -0700

    Start writing SKI combinator calculus question

--- HWDraft/HW3.lean
+++ HWDraft/HW3.lean
@@ -51,3 +51,32 @@ Problem Y: More practice with logic
 
 -- Prove the following without law of the excluded middle:
 example : ¬¬(p ∨ ¬p) := sorry
+
+/-===========================
+Problem X: SKI Combinator Calculus and Hilbert Style Deduction
+===========================-/
+
+-- If you have these three functions, you can prove a lot of theorems without using the `fun` keyword.
+theorem S {A B C : Prop} : (A → (B → C)) → ((A → B) → (A → C)) := fun x y z ↦ x z (y z)
+theorem K {A B : Prop} : A → B → A := fun x y ↦ x
+theorem I {A : Prop} : A → A := fun x ↦ x
+
+variable {A B C D E F : Prop}
+
+/- According to
+https://en.wikipedia.org/wiki/SKI_combinator_calculus#Conversion_of_lambda_terms_to_SKI_combinators
+you can convert any proof that uses purely `fun` to a proof without `fun` but only `S`, `K`, and `I`
+by repeatedly applying the following 4 rules:
+
+- Replace `fun x ↦ x` with `I`
+- If the expression `e₁` does not contain `x`, replace `fun x ↦ e₁` with `K e₁`
+- If the expression `e₁` does not contain `x`, replace `fun x ↦ e₁ x` with `e₁`.
+- Replace `fun x ↦ e₁ e₂` with `S (fun x ↦ e₁) (fun x ↦ e₂)`.
+-/
+
+-- For example:
+example : A → (A → B) → B := fun x ↦ fun y ↦ y x
+example : A → (A → B) → B := fun x ↦ (S (fun y ↦ y)) (fun y ↦ x)
+example : A → (A → B) → B := fun x ↦ (S I) (K x)
+example : A → (A → B) → B := S (fun x ↦ S I) (fun x ↦ K x)
+example : A → (A → B) → B := S (K (S I)) K
